8add5ab1b197309aba16a5e386315b5e01e3926e
farsinalifr7 <farsinalifr7@gmail.com>
2024-05-31 13:51:41 +0530

    fetch data form backend

--- lib/pages/homepage/screen/homepage/home_page.dart
+++ lib/pages/homepage/screen/homepage/home_page.dart
@@ -2,11 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:studentmodelhive/functions/fetch_details.dart';
+import 'package:studentmodelhive/model/rate_model.dart';
 import 'package:studentmodelhive/pages/homepage/components/convert_to_any.dart';
 import 'package:studentmodelhive/pages/homepage/components/usd_to_any.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late Future<RateModel> result;
+  late Future<Map> allCurrencies;
+  @override
+  void initState() {
+    super.initState();
+    result = fetchRate();
+    allCurrencies = fetchCurrency();
+    print(result);
+    print(allCurrencies);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -54,65 +72,91 @@ class HomePage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(19),
-        child: Column(
-          children: [
-            const UsdToAny(),
-            const SizedBox(
-              height: 20,
-            ),
-            const ConvertToAny(),
-            const SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "Currency",
-                  style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w400,
-                      color: Colors.grey[800]),
-                ),
-                const Text(
-                  "See all",
-                  style: TextStyle(color: Colors.grey),
-                )
-              ],
-            ),
-            Expanded(
-                child: ListView.builder(
-                    itemCount: 10,
-                    itemBuilder: (context, index) {
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 10),
-                        child: Container(
-                          height: 75,
-                          width: MediaQuery.of(context).size.width,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(8),
-                            color: Colors.white,
-                          ),
-                          child: ListTile(
-                            leading: CircleAvatar(
-                              radius: 27,
-                              backgroundColor: colors[index],
-                              child: const Text(
-                                "AF",
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.w700,
-                                    fontSize: 22),
-                              ),
-                            ),
-                            title: const Text("index"),
-                            subtitle: const Text("subtitle"),
-                          ),
+        child: FutureBuilder<RateModel>(
+            future: result,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(
+                  child: CircularProgressIndicator(
+                    valueColor:
+                        AlwaysStoppedAnimation<Color>(Colors.deepPurple),
+                  ),
+                );
+              }
+
+              return FutureBuilder<Map>(
+                  future: allCurrencies,
+                  builder: (context, currsnapshot) {
+                    if (currsnapshot.connectionState ==
+                        ConnectionState.waiting) {
+                      return const Center(
+                          child: CircularProgressIndicator(
+                        valueColor:
+                            AlwaysStoppedAnimation<Color>(Colors.deepPurple),
+                      ));
+                    }
+                    return Column(
+                      children: [
+                        const UsdToAny(),
+                        const SizedBox(
+                          height: 20,
                         ),
-                      );
-                    }))
-          ],
-        ),
+                        const ConvertToAny(),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "Currency",
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w400,
+                                  color: Colors.grey[800]),
+                            ),
+                            const Text(
+                              "See all",
+                              style: TextStyle(color: Colors.grey),
+                            )
+                          ],
+                        ),
+                        Expanded(
+                            child: ListView.builder(
+                                itemCount: 10,
+                                itemBuilder: (context, index) {
+                                  return Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        vertical: 10),
+                                    child: Container(
+                                      height: 75,
+                                      width: MediaQuery.of(context).size.width,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(8),
+                                        color: Colors.white,
+                                      ),
+                                      child: ListTile(
+                                        leading: CircleAvatar(
+                                          radius: 27,
+                                          backgroundColor: colors[index],
+                                          child: const Text(
+                                            "AF",
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.w700,
+                                                fontSize: 22),
+                                          ),
+                                        ),
+                                        title: const Text("index"),
+                                        subtitle: const Text("subtitle"),
+                                      ),
+                                    ),
+                                  );
+                                }))
+                      ],
+                    );
+                  });
+            }),
       ),
     );
   }
